fc18633fbf1e11724b650439036286e42d0ac781
mingwiki <mingwiki@gmail.com>
2022-12-06 18:27:24 +0800

    Update useStore.js

--- useStore.js
+++ useStore.js
@@ -2,21 +2,19 @@ import { useState, useRef, useCallback, useEffect, useReducer } from 'react';
 
 const publicState = {};
 
-const reducer = (state, payload) => {
-  return {
-    ...state,
-    ...payload,
-  };
-};
+const reducer = (state, payload) => ({ ...state, ...payload });
 
 const useStore = (privateStore = {}) => {
   const [payload, setPayload] = useState(false);
   const callbackRef = useRef();
   const isFirstCallbackCall = useRef(true);
-  const [state, dispatch] = useReducer(reducer, { ...publicState, ...privateStore });
+  const [state, dispatch] = useReducer(reducer, {
+    ...publicState,
+    ...privateStore,
+  });
   const setState = useCallback((payload, callback) => {
     dispatch(payload);
-    callbackRef.current = payload => callback(payload);
+    callbackRef.current = (payload) => callback(payload);
     setPayload(payload);
   }, []);
   useEffect(() => {
